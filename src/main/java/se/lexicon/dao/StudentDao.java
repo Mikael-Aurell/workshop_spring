2e07ec6946b48ccdaaf337b101ea09faf5476fed
Mikael Aurell <aurell.mikael@gmail.com>
2021-02-22 11:29:45 +0100

    Added StudentDao + StudentDaoImpl

--- src/main/java/se/lexicon/dao/StudentDao.java
+++ src/main/java/se/lexicon/dao/StudentDao.java
@@ -1,4 +1,11 @@
 package se.lexicon.dao;
 
+import se.lexicon.model.Student;
+
+import java.util.List;
+
 public interface StudentDao {
+    Student save(Student student);
+    Student find(int id);
+    List<Student> findAll();
 }
--- src/main/java/se/lexicon/dao/StudentDaoImpl.java
+++ src/main/java/se/lexicon/dao/StudentDaoImpl.java
@@ -1,4 +1,22 @@
 package se.lexicon.dao;
 
-public class StudentDaoImpl {
+import se.lexicon.model.Student;
+
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
 }
